d4ec95b43dc5e986703c1bbb66e01029f3679fbd
erikdaniel949 <danielerik949@gmail.com>
2025-11-18 10:16:19 -0300

    Remove duplicate namespace and use statements

--- app/Models/Provincias.php
+++ app/Models/Provincias.php
@@ -10,17 +10,6 @@ use App\Models\Telegramas;
 
 class Provincias extends Model
 {
-
-    namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-use App\Models\Listas;
-use App\Models\Candidatos;
-use App\Models\Mesas;
-use App\Models\Telegramas;
-
-class Provincias extends Model
-{
     // Definición de la clave primaria personalizada
     protected $primaryKey = 'provincia';  // Usamos 'provincia' como clave primaria
 
@@ -49,6 +38,4 @@ class Provincias extends Model
     protected $fillable = [
         'provincia'
     ];
-}
-
 }
